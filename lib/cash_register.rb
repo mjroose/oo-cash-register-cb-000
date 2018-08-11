691a14c90bc60663f513c364f9a9666c6299d473
Michael Roose <mjroose@gmail.com>
2018-08-11 22:09:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -35,7 +35,8 @@ class CashRegister
   end
 
   def void_last_transaction
-    @total -= Float(items[-1][:price])
+    last_item = items[-1][:price]
+    @total = @total - last_item
     items.pop
   end
 end
